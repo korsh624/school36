0c2cc875bdcdb409059940a4bfc53b6ca007d911
korsh624 <korsh624@gmail.com>
2023-11-16 08:40:10 +0300

    add form html

--- basic_web/app.py
+++ basic_web/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 name='name'
 @app.route("/")
@@ -7,6 +7,24 @@ def home():
 @app.route("/allinfo")
 def allinfo():
     return render_template('allinfo.html')
+
+@app.route("/form")
+def form():
+    return render_template('form.html')
+
+
+@app.route('/read-form', methods=['POST'])
+def read_form():
+    # Get the form data as Python ImmutableDict datatype
+    data = request.form
+    ## Return the extracted information
+    return {
+        'emailId': data['userEmail'],
+        'phoneNumber': data['userContact'],
+        'password': data['userPassword'],
+        'gender': 'Male' if data['genderMale'] else 'Female',
+    }
+
 if __name__=="__main__":
     app.run(debug=True)
 
